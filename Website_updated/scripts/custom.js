3e2c43fb56e0d21006077d17f6aa321edcafc3c7
Steve18292 <stephenmurphy08134@gmail.com>
2016-02-22 09:15:57 +0000

    Added in javascript

---
+++ Website_updated/scripts/custom.js
@@ -0,0 +1,37 @@
+// JavaScript Document for Footie Stats
+
+//Show and Hide for shots
+function hideShot() {
+    document.getElementById("mshots").style.visibility = "hidden"; 
+}
+
+function showShot() {
+    document.getElementById("mshots").style.visibility = "visible"; 
+}
+
+//Show and Hide for Passing
+function hidePassing() {
+    document.getElementById("mpassing").style.visibility = "hidden"; 
+}
+
+function showPassing() {
+    document.getElementById("mpassing").style.visibility = "visible"; 
+}
+
+//Show and Hide for Tackling
+function hideTackling() {
+    document.getElementById("mtackles").style.visibility = "hidden"; 
+}
+
+function showTackling() {
+    document.getElementById("mtackles").style.visibility = "visible"; 
+}
+
+//Show and Hide for Info
+function hideInfo() {
+    document.getElementById("minfo").style.visibility = "hidden"; 
+}
+
+function showInfo() {
+    document.getElementById("minfo").style.visibility = "visible"; 
+}
